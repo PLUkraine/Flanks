1df83348ffa52c04a1725a472b1cf32e1bda479b
PLUkraine <plukraine001@gmail.com>
2020-07-26 21:21:22 -0700

    Add basic movement

--- scripts/PlayerTank.gd
+++ scripts/PlayerTank.gd
@@ -1,16 +1,22 @@
-extends Sprite
+extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+export var MAX_SPEED: float = 100.0
+export var MAX_ROTATION_SPEED: float = 1.6
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
     pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
+func _physics_process(delta):
+    var angle = self.rotation
+    var speed = Vector2(cos(angle), sin(angle))
+    print(speed)
+    
+    if Input.is_action_pressed("player1_forward"):
+        self.move_and_collide( speed * MAX_SPEED * delta)
+    if Input.is_action_pressed("player1_backward"):
+        self.move_and_collide(-speed * MAX_SPEED * delta)
+    if Input.is_action_pressed("player1_left"):
+        self.rotate(-MAX_ROTATION_SPEED*delta)
+    if Input.is_action_pressed("player1_right"):
+        self.rotate( MAX_ROTATION_SPEED*delta)
